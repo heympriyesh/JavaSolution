52587ec413b50e104f4d154694869a2301a24617
unknown <priyesh.pandey321@gmail.com>
2020-10-21 23:48:23 +0530

    Java Solution

---
+++ Kth Smallest number.java
@@ -0,0 +1,24 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++)
+            {
+                arr[i]=sc.nextInt();
+            }
+            int k=sc.nextInt();
+            Arrays.sort(arr);
+            System.out.println(arr[k-1]);
+        }
+	}
+}

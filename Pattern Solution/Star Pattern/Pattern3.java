c995bc13a86e7ccdec05895a2ac7e4811cc4be3b
unknown <priyesh.pandey321@gmail.com>
2020-10-31 23:19:17 +0530

    Java Solution

--- Pattern Solution/Star Pattern/Pattern3.java
+++ Pattern Solution/Star Pattern/Pattern3.java
@@ -21,7 +21,7 @@ public class MyClass {
       }
       for(int i=1;i<=5;i++)
       {
-          for(int j=5;j>=i;j--)
+          for(int j=4;j>=i;j--)
           {
               System.out.print("*");
           }

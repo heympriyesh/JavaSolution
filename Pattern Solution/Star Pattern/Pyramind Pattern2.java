222bfc16794f96b2c87202cce25546a76e1213d1
unknown <priyesh.pandey321@gmail.com>
2020-10-31 23:56:06 +0530

    Java Solution

---
+++ Pattern Solution/Star Pattern/Pyramind Pattern2.java
@@ -0,0 +1,25 @@
+//     *
+//   ***
+//   *****
+//  *******
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        for(int i=1;i<=4;i++)
+        {
+            for(int j=4;j>=i;j--)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++)
+            {
+                System.out.print("*");
+            }
+            for(int l=2;l<=i;l++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

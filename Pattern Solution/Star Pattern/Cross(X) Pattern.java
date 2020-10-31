9831150b302bf96166060e21ae47b7974a51108b
unknown <priyesh.pandey321@gmail.com>
2020-11-01 01:40:41 +0530

    Java Solution

---
+++ Pattern Solution/Star Pattern/Cross(X) Pattern.java
@@ -0,0 +1,22 @@
+
+*   *
+ * * 
+  *  
+ * * 
+*   *
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                if(i==j || i+j==(5-1))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}

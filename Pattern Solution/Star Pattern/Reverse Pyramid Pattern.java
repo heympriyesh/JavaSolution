a7c1c82b82e9c3ecec11e75cefbf3fabb4cdeb03
unknown <priyesh.pandey321@gmail.com>
2020-11-01 00:03:46 +0530

    Java Solution

--- Pattern Solution/Star Pattern/Reverse Pyramid Pattern.java
+++ Pattern Solution/Star Pattern/Reverse Pyramid Pattern.java
@@ -1,17 +1,21 @@
-//     *
-//   ***
-//   *****
 //  *******
+//   *****
+//   ***
+//     *
 import java.util.*;
 public class MyClass {
     public static void main(String args[]) {
         for(int i=1;i<=4;i++)
         {
-            for(int j=4;j>=i;j--)
+            for(int j=1;j<=i;j++)
             {
                 System.out.print(" ");
             }
-            for(int k=1;k<(i*2);k++)
+            for(int k=4;k>=i;k--)
+            {
+                System.out.print("*");
+            }
+            for(int l=3;l>=i;l--)
             {
                 System.out.print("*");
             }

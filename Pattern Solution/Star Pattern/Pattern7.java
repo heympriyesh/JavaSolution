059d8b94c66564739673f5e11352c8fa644e9ca6
unknown <priyesh.pandey321@gmail.com>
2020-11-01 01:17:18 +0530

    Java Solution

---
+++ Pattern Solution/Star Pattern/Pattern7.java
@@ -0,0 +1,22 @@
+*
+ *
+  *
+   *
+    *
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                if(i>=2 && j<=i-1)
+                System.out.print(" ");
+                else
+                    System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
